9883a0e96912e0a2f6065060d5a8e572f392268d
jiwei-hemeng <jiwei7065@sina.com>
2021-05-21 18:16:25 +0800

    项目中使用scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Request from "@/utils/request";
 import Loadding from "@/component/loadding";
+import "./App.scss";
 export default class App extends React.Component {
   async componentDidMount() {
     const { data } = await Request({
@@ -10,7 +11,10 @@ export default class App extends React.Component {
     console.log("lunbotu", data);
   }
   render() {
-    // return <div className="App">12312323</div>;
-    return <Loadding />;
+    return (
+      <div className="App">
+        <Loadding />
+      </div>
+    );
   }
 }
