0fa6dda1283b1ec1b72bff992609d43799e57d83
jiwei-hemeng <jiwei7065@sina.com>
2021-06-10 18:21:41 +0800

    生产环境下按需加载

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://192.168.1.168",
+      target: "http://192.168.1.168:3006",
       // target: "https://www.bilibili.com",
       changeOrigin: true,
       pathRewrite: {
