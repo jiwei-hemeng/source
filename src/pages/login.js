e4cc085fdb8134f9a6e38e928e852121c10699f2
jiwei-hemeng <jiwei7065@sina.com>
2021-06-19 15:48:22 +0800

    登录组件

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Login extends React.Component{
+  state = {
+    userName: "hemeng",
+    passWord: "12332"
+  }
+  render() {
+    return (
+      <div>这是登录组件</div>
+    )
+  }
+}
