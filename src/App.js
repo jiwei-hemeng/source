9d04185d8e73c03b2502728af81c7bb75c74198f
jiwei-hemeng <jiwei7065@sina.com>
2021-06-19 15:47:04 +0800

    路由重定向

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import Loadding from "@/component/loadding"
 const Index = React.lazy(() => import("./pages/"));
-const Home = React.lazy(() => import("@/pages/home"))
+const Login = React.lazy(() => import("@/pages/login"))
 export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Suspense fallback={<Loadding />}>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" render={() => {
+            return <Redirect to="/home/index"></Redirect>
+          }} />
+          <Route path="/home" component={Index} />
+          <Route exact path="/login" component={Login} />
         </Suspense>
       </BrowserRouter>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Route } from "react-router-dom";
+const Home = React.lazy(() => import("@/pages/home"))
 class Index extends React.Component {
   render() {
     return (
-      <div>首页</div>
+      <Route exact path="/home/index" component={Home} />
     );
   }
 }
