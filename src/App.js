50927ec0be5b59fbb9841988d29afe115b1dca6b
jiwei-hemeng <jiwei7065@sina.com>
2021-06-23 10:57:03 +0800

    将路由模式改为hash模式

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+// import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { HashRouter, Route, Redirect } from "react-router-dom";
 import Loadding from "@/component/loadding"
 const Index = React.lazy(() => import("./pages/"));
 const Login = React.lazy(() => import("@/pages/login"))
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      // <BrowserRouter>
+      //   <Suspense fallback={<Loadding />}>
+      //     <Route exact path="/" render={() => {
+      //       return <Redirect to="/home/index"></Redirect>
+      //     }} />
+      //     <Route path="/home" component={Index} />
+      //     <Route exact path="/login" component={Login} />
+      //   </Suspense>
+      // </BrowserRouter>
+      <HashRouter>
         <Suspense fallback={<Loadding />}>
           <Route exact path="/" render={() => {
             return <Redirect to="/home/index"></Redirect>
@@ -14,7 +24,7 @@ export default class App extends React.Component {
           <Route path="/home" component={Index} />
           <Route exact path="/login" component={Login} />
         </Suspense>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
