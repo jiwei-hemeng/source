2cb4bd60eda25fb94e26e1f30556dfbfc83f9555
jiwei-hemeng <jiwei7065@sina.com>
2021-06-29 14:20:16 +0800

    解决tarbar定位，以及添加以后botton点击无效

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,48 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import style from "./index.module.scss";
+// import style from "./index.module.scss";
+import { TabBar } from "antd-mobile";
 const Home = React.lazy(() => import("@/pages/home"));
 class Index extends React.Component {
-  clickItem = (i) => {
-    console.log("点击了第" + i + "项")
+  state = {
+    selectedTab: "order"
   }
   render() {
     return (
       <>
         <Route exact path="/home/index" component={Home} />
-        <ul className={style.tarbar}>
-          <li 
-            className={style.tarbar_item} 
-            onClick={() => {
-              this.clickItem(0)
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+          prerenderingSiblingsNumber="0"
+          tabBarPosition="bottom"
+        >
+          <TabBar.Item
+            title="正常订单"
+            key="order"
+            icon={<i className="iconfont icon-order" />}
+            selectedIcon={<i className="iconfont icon-order" />}
+            selected={this.state.selectedTab === 'order'}
+            onPress={() => {
+              this.setState({
+                selectedTab: "order",
+              });
             }}
-          >
-            <i className="iconfont icon-order"></i>
-            <p>正常订单</p>
-          </li>
-          <li 
-            className={style.tarbar_item}
-            onClick={() => {
-              this.clickItem(1)
+          ></TabBar.Item>
+          <TabBar.Item
+            title="逾期订单"
+            key="order"
+            icon={<i className="iconfont icon-overdue" />}
+            selectedIcon={<i className="iconfont icon-overdue" />}
+            selected={this.state.selectedTab === 'overdue'}
+            onPress={() => {
+              this.setState({
+                selectedTab: "overdue",
+              });
             }}
-          >
-            <i className="iconfont icon-overdue"></i>
-            <p>逾期订单</p>
-          </li>
-        </ul>
+          />
+        </TabBar>
       </>
     );
   }
