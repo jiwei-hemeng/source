6d7bc0c02fb3f9a48569695326d89c839cbe9f0a
jiwei-hemeng <jiwei7065@sina.com>
2021-07-30 17:30:08 +0800

    123

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,10 @@
-const {
-  createProxyMiddleware
-} = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
       // target: "http://xsmovetest.xiangshangkj.com:8066/",
-      target: "http://move.laotielian.com:8066/",
-      // target: "http://192.168.1.106:7066/",
+      // target: "http://move.laotielian.com:8066/",
+      target: "http://192.168.1.119:7066",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
