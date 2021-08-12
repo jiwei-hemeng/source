caf6ad5c97efb316f9f021c9a99972e0aa6e748e
jiwei <62381906+jiwei-hemeng@users.noreply.github.com>
2021-08-12 16:25:51 +0800

    123

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import store from "./store/index.js";
 import "lib-flexible";
 import "./index.scss";
-import "./iconfont.css";
 import "antd-mobile/dist/antd-mobile.css";
 ReactDOM.render(
   <Provider store={store}>
