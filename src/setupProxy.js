1c3e73ba715f72e417a6232bb034596f763891db
jiwei <62381906+jiwei-hemeng@users.noreply.github.com>
2021-08-16 10:50:47 +0800

    123

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://xsmove.xiangshangkj.com:8066/",
-      // target: "http://move.laotielian.com:8066/",
+      // target: "http://xsmove.xiangshangkj.com:8066/",
+      target: "http://move.laotielian.com:8066/",
       // target: "http://192.168.1.119:7066",
       changeOrigin: true,
       pathRewrite: {
